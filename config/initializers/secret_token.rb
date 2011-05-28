f3cff557bb90ce1eeb18f9600b7e93757b1d4660
Nick <nick@nick-desktop.(none)>
2011-05-27 21:49:24 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForeCashed::Application.config.secret_token = '438f31085915081096bc44beec4a0f3b79e3961bae7cf44858bdb3062ec30addc3c294f33deb294918d459f60d1ac10aed36e1062b4866e20e88f5e7a781f3c7'
